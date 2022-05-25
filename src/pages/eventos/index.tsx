1eb8d700207be118df60dfda2f666789c128805e
percy cordova <cordova_inf@hotmail.com>
2022-05-24 22:35:28 -0500

    agregando la vista de eventos y la esperanza

--- src/pages/eventos/index.tsx
+++ src/pages/eventos/index.tsx
@@ -1,7 +1,65 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Eventos = () => {
-  return <div className='banner banner-home'></div>
+  return (
+    <div className='grid px-5 pb-10 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-[1200px]  mx-auto text-center text-[#505050] mt-32'>
+      <article className='max-w-[300px] mx-auto sm:mx-0 sm:max-w-none'>
+        <Image
+          src='/imgs/eventos/evento-01.jpg'
+          alt='Picture of the author'
+          width={500}
+          height={350}
+          className='object-cover'
+        />
+        <h6 className='text-[#4c000c] font-bold text-4xl text-center my-5'>Oscar Quiñonez</h6>
+        <p className=' text-left'>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+          been the industry’s standard dummy text ever since the 1500s, when an unknown printer took
+          a galley of type and scrambled it to make a type specimen book.
+        </p>
+        <button className='bg-[#a02e2b] text-white pb-2 pt-1 mt-5 rounded-sm px-4 hover:opacity-75 transition-all duration-500'>
+          Comprar entrada
+        </button>
+      </article>
+      <article className='max-w-[300px] mx-auto sm:mx-0 sm:max-w-none'>
+        <Image
+          src='/imgs/eventos/evento-02.jpg'
+          alt='Picture of the author'
+          width={500}
+          height={350}
+          className='object-cover'
+        />
+        <h6 className='text-[#4c000c] font-bold text-4xl text-center my-5'>Miguel Quiñonez</h6>
+        <p className=' text-left'>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+          been the industry’s standard dummy text ever since the 1500s, when an unknown printer took
+          a galley of type and scrambled it to make a type specimen book.
+        </p>
+        <button className='bg-[#a02e2b] text-white pb-2 pt-1 mt-5 rounded-sm px-4 hover:opacity-75 transition-all duration-500'>
+          Comprar entrada
+        </button>
+      </article>
+      <article className='max-w-[300px] mx-auto sm:mx-0 sm:max-w-none'>
+        <Image
+          src='/imgs/eventos/evento-01.jpg'
+          alt='Picture of the author'
+          width={500}
+          height={350}
+          className='object-cover'
+        />
+        <h6 className='text-[#4c000c] font-bold text-4xl text-center my-5'>Julian Quiñonez</h6>
+        <p className=' text-left'>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+          been the industry’s standard dummy text ever since the 1500s, when an unknown printer took
+          a galley of type and scrambled it to make a type specimen book.
+        </p>
+        <button className='bg-[#a02e2b] text-white pb-2 pt-1 rounded-sm px-4 hover:opacity-75 transition-all duration-500 mt-5'>
+          Comprar entrada
+        </button>
+      </article>
+    </div>
+  )
 }
 
 export default Eventos
--- src/pages/plaza/index.tsx
+++ src/pages/plaza/index.tsx
@@ -1,10 +1,119 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import Image from 'next/image'
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
 
+// import required modules
+import { Navigation, Pagination } from 'swiper'
 const Plaza = () => {
   return (
-    <div className='banner banner-plaza'>
-      <div>La plaza</div>
-    </div>
+    <>
+      <div className='banner banner-plaza relative'>
+        <p className='absolute top-[40%]  right-[30%]  sm:right-[40%] text-5xl sm:text-6xl font-bold text-white'>
+          La Plaza
+        </p>
+      </div>
+      <div className='bg-[#fff2e6] py-6'>
+        <div className='max-w-[1200px] px-4 mx-auto text-center text-[#505050] mb-4 '>
+          <p className='max-w-3xl mx-auto'>
+            La Plaza de Toros La Esperanza fue inaugurada en el año 2009, siendo un ruedo sin
+            graderías, el punto de encuentro y lugar donde se incubó la nueva generación de
+            aficionados prácticos, llenando aquel año cada sábado con encerronas taurinas y gratos
+            momentos de confraternidad.
+          </p>
+          <br />
+          <p className='max-w-3xl mx-auto'>
+            Buscando ser la casa del taurino de buena voluntad, se amplió en una primera etapa
+            dotándola de un callejón que permita el buen desarrollo de un festejo taurino y cómodas
+            butacas en las tres filas más cercanas al ruedo. Bajo esa configuración se albergaron
+            las primeras corridas de toros formales brindando un lugar muy especial a la afición
+            taurina, que fue creando el ambiente y la identidad de La Esperanza, en un espacio de
+            campo con sabor criollo y fraternidad.
+          </p>
+          <br />
+          <p className='max-w-3xl mx-auto mb-4'>
+            Y así, la necesidad de albergar más afición tras vivir esta pandemia hizo que la plaza
+            creciera mucho más, contando ahora con un tendido fijo de 12 filas y 5 palcos, capaz de
+            albergar en su totalidad cerca de 3000 personas, manteniendo su coqueta identidad y esa
+            personalidad que invita a disfrutar una tarde de toros entre amigos.
+          </p>
+          <Swiper
+            style={{
+              '--swiper-navigation-color': '#4c000c',
+              // '--swiper-navigation-size': '20px'
+              '--swiper-pagination-color': '#4c000c',
+              '--swiper-pagination-active-color': '#4c000c'
+              //'--swiper-pagination-background': '#fff',
+              //'--swiper-pagination-active-background': '#fff',
+              //'--swiper-pagination-border-color': '#fff',
+              //'--swiper-pagination-size': '10px',
+              //'--swiper-pagination-spacing': '10px',
+              //'--swiper-pagination-bullet-size': '10px'
+            }}
+            slidesPerView={1}
+            spaceBetween={30}
+            navigation={true}
+            loop={true}
+            pagination={{
+              clickable: true
+            }}
+            breakpoints={{
+              640: {
+                slidesPerView: 1,
+                spaceBetween: 20
+              },
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 10
+              },
+              1024: {
+                slidesPerView: 3,
+                spaceBetween: 0
+              }
+            }}
+            modules={[Navigation, Pagination]}
+            className='mySwiper'
+          >
+            <SwiperSlide>
+              <div className=' pb-10 pt-3'>
+                <Image
+                  src='/imgs/la-esperanza/la-esperanza-01.jpg'
+                  alt='Picture of the author'
+                  width={500}
+                  height={250}
+                  className='object-cover'
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className=' pb-10 pt-3'>
+                <Image
+                  src='/imgs/la-esperanza/la-esperanza-02.jpeg'
+                  alt='Picture of the author'
+                  width={500}
+                  height={250}
+                  className='object-cover'
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className=' pb-10 pt-3'>
+                <Image
+                  src='/imgs/la-esperanza/la-esperanza-03.jpg'
+                  alt='Picture of the author'
+                  width={500}
+                  height={250}
+                  className='object-cover'
+                />
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
+    </>
   )
 }
 
