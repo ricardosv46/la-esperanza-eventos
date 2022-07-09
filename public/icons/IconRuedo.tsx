45c3c3f780b185219403f9c77b6e4ba936056db6
RICARDO SOLIS <ricardosv46@gmail.com>
2022-07-09 17:48:28 -0500

    feat: se termina modal tendido 1

---
+++ public/icons/IconRuedo.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import { SVGProps } from 'react'
+
+const IconRuedo = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width={400}
+    height={100}
+    id='Capa_1'
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 821.57 100.19'
+    {...props}
+  >
+    <defs>
+      <style>
+        {
+          '.cls-1{fill:#575757;font-size:6px;}.cls-1,.cls-2,.cls-3{font-family:Lato-Bold, Lato;}.cls-4{letter-spacing:0em;}.cls-5{fill:#717171;}.cls-2{fill:#984034;font-size:12px;}.cls-6{fill:#cdcdcd;stroke:#3a3a3a;stroke-miterlimit:10;stroke-width:.5px;}.cls-7{letter-spacing:-.03em;}.cls-3{fill:#fff;font-size:20px;}'
+        }
+      </style>
+    </defs>
+    <path
+      className='cls-5'
+      d='M670.92,156.13H140.85v-23.93l47.24-3.13c145.76-9.64,291.84-9.61,437.59,.11l45.25,3.02v23.93Z'
+    />
+    <text className='cls-3' transform='translate(386.61 145.27)'>
+      <tspan className='cls-7' x={0} y={0}>
+        {'R'}
+      </tspan>
+      <tspan className='cls-4' x={12.59} y={0}>
+        {'UEDO'}
+      </tspan>
+    </text>
+  </svg>
+)
+
+export default IconRuedo
--- src/components/asientos/index.tsx
+++ src/components/asientos/index.tsx
@@ -1,5 +1,6 @@
 import React, { Dispatch, SetStateAction, useMemo, useState } from 'react'
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch'
+import IconRuedo from '../../../public/icons/IconRuedo'
 
 import IconSearchMinus from '../../../public/icons/IconSearchMinus'
 import IconSearchPlus from '../../../public/icons/IconSearchPlus'
@@ -8,11 +9,13 @@ interface IProps {
   seleccionados: IColums[]
   setSeleccionados: Dispatch<SetStateAction<IColums[]>>
   desabilitados: string[]
+  nombreFilas: string[]
 }
 
 interface Filas {
   fila: string
   columnas: number
+  precio: number
 }
 
 export interface IColums {
@@ -26,19 +29,21 @@ const Asientos = ({
   seleccionados,
   setSeleccionados,
   data,
-  desabilitados
+  desabilitados,
+  nombreFilas
 }: IProps) => {
   const filas = useMemo(() => {
     let tfilas: any = {}
     for (let i = 0; i < data.length; i++) {
       const fila = data[i].fila
+      const precio = data[i].precio
       tfilas[data[i].fila] = new Array(data[i].columnas)
         .fill(null)
         .map((_, i) => ({
           id: `${fila}${i + 1}`,
           estado: 'Disponible',
           numero: i + 1,
-          precio: 50
+          precio: precio
         }))
     }
     return tfilas
@@ -55,60 +60,89 @@ const Asientos = ({
   }
 
   return (
-    <div className='flex gap-3 justify-start overflow-x-scroll no-scrollbar '>
+    <div className='flex gap-3 justify-start overflow-x-scroll  overflow-y-scroll no-scrollbar pt-3 '>
       <TransformWrapper initialScale={1}>
         {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
-          <div className='overflow-x-scroll no-scrollbar flex flex-col items-center  w-full '>
-            <div className=' overflow-x-hidden '>
-              <TransformComponent
-                contentStyle={{
-                  padding: '40px 20px'
-                }}
-              >
-                <div className='flex flex-col gap-3 justify-center items-center '>
-                  {Object.keys(filas).map((fila) => (
-                    <div
-                      key={fila}
-                      className='flex justify-center items-center gap-1'
+          <div className='overflow-x-scroll  overflow-y-scroll  no-scrollbar flex flex-col items-center  w-full  '>
+            <div className=' w-full overflow-x-scroll custom-scroll snap-section'>
+              <TransformComponent>
+                <div className='py-10'>
+                  <div className='flex flex-col gap-3 justify-center items-center  px-5 w-full '>
+                    {Object.keys(filas).map((fila, index) => (
+                      <div
+                        key={fila}
+                        className='flex justify-between items-center gap-5 w-full '
+                      >
+                        <div className='w-36 lg:w-24  text-right'>
+                          <p className=' text-base lg:text-[10px] text-primary font-semibold'>
+                            {nombreFilas[index]}
+                          </p>
+                        </div>
+                        <div className='flex flex-row-reverse justify-center items-center gap-1 flex-1'>
+                          {filas[`${fila.toString()}`].map(
+                            (
+                              { id, estado, precio, numero }: IColums,
+                              index: any
+                            ) => {
+                              if (index < numero) {
+                                const isActive = seleccionados.some(
+                                  (seleccionado) => seleccionado.id === id
+                                )
+                                const disabled = desabilitados.some(
+                                  (_id) => _id === id
+                                )
+                                return (
+                                  <button
+                                    id={id}
+                                    key={id}
+                                    onClick={() =>
+                                      selectId({ id, estado, precio, numero })
+                                    }
+                                    disabled={disabled}
+                                    className={`
+                                  ${
+                                    disabled
+                                      ? 'bg-text text-white'
+                                      : isActive
+                                      ? 'bg-primary text-white'
+                                      : 'bg-yellow-500  text-primary'
+                                  }
+                                   rounded-full h-7 w-7 lg:h-3.5 lg:w-3.5  font-semibold  flex justify-center items-center lg:text-[7px] text-base `}
+                                  >
+                                    {numero}
+                                  </button>
+                                )
+                              } else return null
+                            }
+                          )}
+                        </div>
+                        <div className='w-36 lg:w-24 '>
+                          <p className='text-base lg:text-[10px] text-primary font-semibold'>
+                            {nombreFilas[index]}
+                          </p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                  <div className='h-14 overflow-hidden relative mt-5 mx-48 lg:mx-36'>
+                    <svg
+                      viewBox='0 0 500 150'
+                      preserveAspectRatio='none'
+                      className='w-full h-full'
                     >
-                      {filas[`${fila.toString()}`].map(
-                        (
-                          { id, estado, precio, numero }: IColums,
-                          index: any
-                        ) => {
-                          if (index < numero) {
-                            const isActive = seleccionados.some(
-                              (seleccionado) => seleccionado.id === id
-                            )
-                            const disabled = desabilitados.some(
-                              (_id) => _id === id
-                            )
-                            return (
-                              <button
-                                id={id}
-                                key={id}
-                                onClick={() =>
-                                  selectId({ id, estado, precio, numero })
-                                }
-                                disabled={disabled}
-                                className={`${
-                                  isActive && 'bg-primary text-white'
-                                } ${
-                                  disabled && 'bg-text text-white'
-                                } rounded-full h-2.5 w-2.5 bg-yellow-500 flex justify-center items-center`}
-                              >
-                                <div className='text-[3px] leading-5'>{id}</div>
-                              </button>
-                            )
-                          } else return null
-                        }
-                      )}
+                      <path
+                        d='M0.00,49.98 C-71.95,55.77 250.27,-17.25 500.00,49.98 L500.00,120.00 L-0.27,117.94 Z'
+                        className='fill-text '
+                      ></path>
+                    </svg>
+                    <div className='absolute top-0 z-10 text-white flex justify-center items-center  w-full h-full '>
+                      RUEDO
                     </div>
-                  ))}
+                  </div>
                 </div>
               </TransformComponent>
             </div>
-            <div className='flex w-full justify-end gap-3 '>
+            <div className='flex w-full justify-end gap-3 pt-2'>
               <button onClick={() => zoomIn()}>
                 <IconSearchPlus
                   fill='#4C000C'
