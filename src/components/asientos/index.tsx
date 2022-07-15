f7bd28f1f16debebf712b66f09616689887f24c5
RICARDO SOLIS <ricardosv46@gmail.com>
2022-07-15 11:52:56 -0500

    feat: se termina cambio de modal a vista

--- src/components/asientos/index.tsx
+++ src/components/asientos/index.tsx
@@ -10,7 +10,8 @@ interface IProps {
   setSeleccionados: Dispatch<SetStateAction<IColums[]>>
   desabilitados: string[]
   nombreFilas: string[]
-  direccion?: "start" | "center" | "end" // default: "center"  start: es al final por el reverse y end: es al inicio
+  doble?: 'Ruedo' | 'Tendido2' | 'Tendido3'
+  direccion?: 'start' | 'center' | 'end' // default: "center"  start: es al final por el reverse y end: es al inicio
 }
 
 interface Filas {
@@ -33,6 +34,7 @@ const Asientos = ({
   desabilitados,
   nombreFilas,
   direccion = 'center',
+  doble = 'Ruedo'
 }: IProps) => {
   const filas = useMemo(() => {
     let tfilas: any = {}
@@ -62,11 +64,11 @@ const Asientos = ({
   }
 
   return (
-    <div className='flex gap-3 justify-start overflow-x-scroll  overflow-y-scroll no-scrollbar pt-3 '>
+    <div className='flex gap-3 justify-start pt-3 '>
       <TransformWrapper initialScale={1}>
         {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
-          <div className='overflow-x-scroll  overflow-y-scroll  no-scrollbar flex flex-col items-center  w-full  '>
-            <div className=' w-full overflow-x-scroll custom-scroll snap-section'>
+          <div className='flex flex-col items-center  w-full'>
+            <div className=' w-full snap-section overflow-x-auto custom-scroll max-w-[95vw]'>
               <TransformComponent>
                 <div className='py-10'>
                   <div className='flex flex-col  justify-center items-center  px-5 w-full '>
@@ -80,7 +82,9 @@ const Asientos = ({
                             {nombreFilas[index]}
                           </p>
                         </div>
-                        <div className={`flex flex-row-reverse justify-${direccion} items-center gap-1 flex-1`}>
+                        <div
+                          className={`flex flex-row-reverse justify-${direccion} items-center gap-1 flex-1`}
+                        >
                           {filas[`${fila.toString()}`].map(
                             (
                               { id, estado, precio, numero }: IColums,
@@ -102,13 +106,14 @@ const Asientos = ({
                                     }
                                     disabled={disabled}
                                     className={`
-                                  ${disabled
-                                        ? 'bg-text text-white'
-                                        : isActive
-                                          ? 'bg-primary text-white'
-                                          : 'bg-yellow-500  text-primary'
-                                      }
-                                   rounded-full h-5 w-5 lg:h-2 lg:w-2  font-semibold  flex justify-center items-center lg:text-[2px] text-[10px] `}
+                                  ${
+                                    disabled
+                                      ? 'bg-text text-white'
+                                      : isActive
+                                      ? 'bg-primary text-white'
+                                      : 'bg-yellow-500  text-primary'
+                                  }
+                                   rounded-full h-5 w-5 lg:h-3 lg:w-3  font-semibold  flex justify-center items-center lg:text-[5px] text-[10px] `}
                                   >
                                     {numero}
                                   </button>
@@ -126,19 +131,46 @@ const Asientos = ({
                     ))}
                   </div>
                   <div className='h-14 overflow-hidden relative mt-5 mx-48 lg:mx-36'>
-                    <svg
-                      viewBox='0 0 500 150'
-                      preserveAspectRatio='none'
-                      className='w-full h-full'
-                    >
-                      <path
-                        d='M0.00,49.98 C-71.95,55.77 250.27,-17.25 500.00,49.98 L500.00,120.00 L-0.27,117.94 Z'
-                        className='fill-text '
-                      ></path>
-                    </svg>
-                    <div className='absolute top-0 z-10 text-white flex justify-center items-center  w-full h-full '>
-                      RUEDO
-                    </div>
+                    {doble === 'Tendido3' && (
+                      <div className='flex justify-between gap-5'>
+                        <div className='bg-text w-full  h-9'>
+                          <p className=' text-white flex justify-center items-center  w-full h-full'>
+                            TENDIDO 3B
+                          </p>
+                        </div>
+                        <div className='bg-text w-full  h-9'>
+                          <p className=' text-white flex justify-center items-center  w-full h-full'>
+                            TENDIDO 3A
+                          </p>
+                        </div>
+                      </div>
+                    )}
+                    {doble === 'Tendido2' && (
+                      <div className='flex'>
+                        <div className='bg-text w-full  h-9'>
+                          <p className=' text-white flex justify-center items-center  w-full h-full'>
+                            TENDIDO 2 BAJO
+                          </p>
+                        </div>
+                      </div>
+                    )}
+                    {doble === 'Ruedo' && (
+                      <>
+                        <svg
+                          viewBox='0 0 500 150'
+                          preserveAspectRatio='none'
+                          className='w-full h-full'
+                        >
+                          <path
+                            d='M0.00,49.98 C-71.95,55.77 250.27,-17.25 500.00,49.98 L500.00,120.00 L-0.27,117.94 Z'
+                            className='fill-text '
+                          ></path>
+                        </svg>
+                        <div className='absolute top-0 z-10 text-white flex justify-center items-center  w-full h-full '>
+                          RUEDO
+                        </div>
+                      </>
+                    )}
                   </div>
                 </div>
               </TransformComponent>
