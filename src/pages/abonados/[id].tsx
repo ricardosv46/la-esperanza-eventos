312a4cebe143efe31c835f459ecf98c749903657
RICARDO SOLIS <ricardosv46@gmail.com>
2022-07-15 12:32:00 -0500

    feat: se agrega nueva ruta de aisentos

---
+++ src/pages/abonados/[id].tsx
@@ -0,0 +1,126 @@
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
+import IconDate from '../../../public/icons/IconDate'
+import Asientos, { IColums } from '../../components/asientos'
+import Container from '../../components/container'
+import { usePaymentContext } from '../../context/payment/PaymentState'
+import { genAsientos } from '../../data/asientos'
+
+const Detalle = () => {
+  const navigation = useRouter()
+  const [dataAsientos, setDataAsientos] = useState({
+    data: [],
+    nombreFilas: [],
+    desabilitados: []
+  })
+  const { data, nombreFilas, desabilitados } = dataAsientos
+
+  console.log(data)
+  const { EnviarPago } = usePaymentContext()
+  const [seleccionados, setSeleccionados] = useState<IColums[]>([])
+  const { id } = useRouter().query as any
+
+  useEffect(() => {
+    const _data = genAsientos(id) as any
+    setDataAsientos(_data)
+  }, [id])
+
+  const total = seleccionados.reduce(
+    (previousValue, currentValue) => previousValue + currentValue.precio,
+    0
+  )
+
+  return (
+    <div className='flex flex-col items-center justify-center w-full'>
+      <section className='bg-white pt-5 w-auto'>
+        <div className='flex justify-center mb-5'>
+          <Image
+            objectFit='scale-down'
+            className='cursor-pointer filter-logo'
+            src='/imgs/logos/logo.png'
+            width={200}
+            height={80}
+            alt='logo'
+          />
+        </div>
+        <p className='text-center text-3xl text-primary font-bold'>
+          SELECCIONA TUS ASIENTOS
+        </p>
+        <div className='flex flex-col justify-center border-b-2 border-t-2 border-primary py-5 mt-5'>
+          <div className='flex justify-between items-center lg:px-8'>
+            <p className=' text-base text-primary font-bold lg:text-xl'>
+              Tendido 1 SOMBRA
+            </p>
+            <div className='flex gap-3 items-center'>
+              <IconDate fill='#4C000C' width={20} height={20} />
+              <p className='text-primary font-bold lg:text-base text-xs'>
+                Sábado 23 de julio
+              </p>
+            </div>
+          </div>
+          {data.length > 0 && (
+            <Asientos
+              {...{
+                data,
+                desabilitados,
+                seleccionados,
+                setSeleccionados,
+                nombreFilas
+              }}
+              doble={
+                id === '2' ? 'Tendido2' : id === '4' ? 'Tendido3' : 'Ruedo'
+              }
+              direccion={id === '5' ? 'end' : id === '6' ? 'start' : 'center'}
+            />
+          )}
+        </div>
+        <div className='p-5 flex gap-3 text-sm lg:text-base'>
+          <div className='flex gap-2 items-center'>
+            <span className='w-2.5 h-2.5 bg-primary rounded-full'></span>
+            <p className='text-tertiary'>Seleccionados</p>
+          </div>
+          <div className='flex gap-2 items-center'>
+            <span className='w-2.5 h-2.5 bg-secondary rounded-full'></span>
+            <p className='text-tertiary'>Libres</p>
+          </div>
+          <div className='flex gap-2 items-center'>
+            <span className='w-2.5 h-2.5 bg-text rounded-full'></span>
+            <p className='text-tertiary'>No disponibles</p>
+          </div>
+        </div>
+      </section>
+      <div className=' bg-secondary  w-full flex justify-center'>
+        <div className='py-10 px-5 max-w-[1200px] flex flex-col lg:flex-row justify-between w-full'>
+          <div className='flex flex-col lg:flex-row gap-5 items-center'>
+            <p className='text-primary font-bold'>Seleccionados:</p>
+            <div className='flex flex-wrap lg:grid lg:grid-cols-10 leading-none gap-2'>
+              {seleccionados.map((item) => (
+                <p key={item.id} className='text-primary text-xs font-bold'>
+                  {item.id}
+                </p>
+              ))}
+            </div>
+          </div>
+          <div className='flex items-end'>
+            <button
+              disabled={seleccionados.length === 0}
+              className='bg-tertiary px-5 py-2 text-white rounded-lg mt-10 lg:mt-0'
+              onClick={() => {
+                navigation.push({
+                  pathname: '/check-out/',
+                  query: { name: 'abono' }
+                })
+                EnviarPago(seleccionados)
+              }}
+            >
+              COMPRAR: S/.{total.toFixed(2)}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Detalle
