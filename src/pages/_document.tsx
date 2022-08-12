ff92a8d89f11919dbae31806c485526f2c3b69f1
ricardosv46 <ricardosv46@gmail.com>
2022-08-12 15:53:42 -0500

    feat: se agrega google analitycs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,23 @@ import { Html, Head, Main, NextScript } from 'next/document'
 const Document = () => {
   return (
     <Html>
-      <Head />
+      <Head>
+        <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-P4TRZH0GH4"
+          />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-P4TRZH0GH4');
+              `,
+            }}
+          />
+      </Head>
       <body>
         <div id="portal" />
         <Main />
